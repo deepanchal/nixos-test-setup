bbe5263ac463174c96d9ec46ab56d6464273feec
Deep Panchal <deep302001@gmail.com>
2024-07-25 20:10:30 -0500

    feat: add impermanence config
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- flake.nix
+++ flake.nix
@@ -42,9 +42,7 @@
         (import ./disk-config.nix {device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_240GB_191386466003";})
 
         ./configuration.nix
-
-        # inputs.home-manager.nixosModules.default
-        # inputs.impermanence.nixosModules.impermanence
+        ./impermanence.nix
       ];
     };
   };
