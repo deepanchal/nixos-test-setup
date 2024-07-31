990d2af73e26e2d0cdfbc8488355f82446fe9e3c
Deep Panchal <deep302001@gmail.com>
2024-07-30 19:49:23 -0500

    feat: take blank root snapshot, restore to pristine state on boot
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -20,42 +20,95 @@
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
 
-  # From: https://github.com/nix-community/impermanence?tab=readme-ov-file#btrfs-subvolumes
-  boot.initrd.postDeviceCommands =
-    lib.mkAfter
-    /*
-    bash
-    */
-    ''
+  boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.services.rollback = {
+    description = "Rollback BTRFS root subvolume to a pristine state";
+    wantedBy = [
+      "initrd.target"
+    ];
+    after = [
+      # LUKS/TPM process
+      # "systemd-cryptsetup@enc.service"
+    ];
+    before = [
+      "sysroot.mount"
+    ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
       echo "impermanence: Starting backup and cleanup procedure"
-      mkdir /btrfs_tmp
-      mount /dev/disk/by-label/NIXOS /btrfs_tmp
-      if [[ -e /btrfs_tmp/@ ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/@ "/btrfs_tmp/old_roots/$timestamp"
-          echo "impermanence: Old root subvolume moved to /btrfs_tmp/old_roots/$timestamp"
-      fi
-
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-          echo "impermanence: Deleted subvolume $1"
-      }
-
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
-
-      btrfs subvolume create /btrfs_tmp/@
-      echo "impermanence: Created new root subvolume at /btrfs_tmp/@"
-
-      umount /btrfs_tmp
+      mkdir -p /mnt
+      # We first mount the btrfs root to /mnt
+      # so we can manipulate btrfs subvolumes.
+      mount -o subvol=/ /dev/disk/by-label/NIXOS /mnt
+      # While we're tempted to just delete /root and create
+      # a new snapshot from /root-blank, /root is already
+      # populated at this point with a number of subvolumes,
+      # which makes `btrfs subvolume delete` fail.
+      # So, we remove them first.
+      #
+      # /root contains subvolumes:
+      # - /root/var/lib/portables
+      # - /root/var/lib/machines
+      #
+      # I suspect these are related to systemd-nspawn, but
+      # since I don't use it I'm not 100% sure.
+      # Anyhow, deleting these subvolumes hasn't resulted
+      # in any issues so far, except for fairly
+      # benign-looking errors from systemd-tmpfiles.
+      btrfs subvolume list -o /mnt/@ |
+        cut -f9 -d' ' |
+        while read subvolume; do
+          echo "impermanence: deleting /$subvolume subvolume..."
+          btrfs subvolume delete "/mnt/$subvolume"
+        done &&
+        echo "impermanence: deleting /root subvolume..." &&
+        btrfs subvolume delete /mnt/@
+      echo "impermanence: restoring blank /root subvolume..."
+      btrfs subvolume snapshot /mnt/@root-blank /mnt/@
+      # Once we're done rolling back to a blank snapshot,
+      # we can unmount /mnt and continue on the boot process.
+      umount /mnt
       echo "impermanence: Done"
     '';
+  };
+
+  # From: https://github.com/nix-community/impermanence?tab=readme-ov-file#btrfs-subvolumes
+  # boot.initrd.postDeviceCommands =
+  #   lib.mkAfter
+  #   /*
+  #   bash
+  #   */
+  #   ''
+  #     echo "impermanence: Starting backup and cleanup procedure"
+  #     mkdir /btrfs_tmp
+  #     mount /dev/disk/by-label/NIXOS /btrfs_tmp
+  #     if [[ -e /btrfs_tmp/@ ]]; then
+  #         mkdir -p /btrfs_tmp/old_roots
+  #         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
+  #         mv /btrfs_tmp/@ "/btrfs_tmp/old_roots/$timestamp"
+  #         echo "impermanence: Old root subvolume moved to /btrfs_tmp/old_roots/$timestamp"
+  #     fi
+  #
+  #     delete_subvolume_recursively() {
+  #         IFS=$'\n'
+  #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+  #             delete_subvolume_recursively "/btrfs_tmp/$i"
+  #         done
+  #         btrfs subvolume delete "$1"
+  #         echo "impermanence: Deleted subvolume $1"
+  #     }
+  #
+  #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+  #         delete_subvolume_recursively "$i"
+  #     done
+  #
+  #     btrfs subvolume create /btrfs_tmp/@
+  #     echo "impermanence: Created new root subvolume at /btrfs_tmp/@"
+  #
+  #     umount /btrfs_tmp
+  #     echo "impermanence: Done"
+  #   '';
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
